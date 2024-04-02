9abf4f7580a98ff35a39628e1d0af304da55dd3b
Daniel octo <danielindra02@gmail.com>
2024-04-02 19:46:20 +0700

    menambahkan services

---
+++ services/firebase.js
@@ -0,0 +1,30 @@
+import { initializeApp } from "firebase/app";
+import {getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut} from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: process.env.NEXT_PUBLIC_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_APP_ID
+};
+
+if(!getApps().length){
+    initializeApp(firebaseConfig);
+}
+
+export const FirebaseAuth = getAuth();
+
+export const signUp = async (email, password) => {
+    await createUserWithEmailAndPassword(FirebaseAuth, email, password);
+}
+
+export const signIn = async (email, password) => {
+    await signInWithEmailAndPassword(FirebaseAuth, email, password);
+}
+
+export const signOut = async () => {
+    await signOut(FirebaseAuth);
+}
+
